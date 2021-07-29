d01931e63224f9b948cd88e04acf59da21847881
freelyfly84 <freelyfly84@gmail.com>
2021-07-29 17:51:43 +0900

    Update kill_process.sh

--- Scripts/kill_process.sh
+++ Scripts/kill_process.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo pkill -f 'java -jar'
+CURRENT_JAVA=`ps -ef | grep java | grep hjh | awk '{print $2}'`
+if [ -z $CURRENT_JAVA ]; then
+	echo "Aleady Stopped"
+else
+       sudo pkill -f 'java -jar'
+fi
